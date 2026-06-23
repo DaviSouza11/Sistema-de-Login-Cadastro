dde6c975828f4dd600e3c6338db4cc70e8058874
Davi Belo de Souza <128198668+DaviSouza11@users.noreply.github.com>
2026-06-23 16:35:54 -0300

    Definição de Rotas e Reestrutura de pastas

--- backend/index.php
+++ backend/index.php
@@ -1,2 +1,56 @@
 <?php
+    session_start();
 
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header('Content-Type: application/json; charset=utf-8');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+        http_response_code(200);
+        exit();
+    }
+
+    define('BASE_DIR', __DIR__ . '/api/');
+
+    //Tabela de rotas
+    $rotas = [
+        //Endpoint Login
+        'api/login' => [ 
+            'file' => 'auth/login.php',
+            'login' => false,
+            'grupos' => null
+    ],
+        //Endpoint Cadastro
+        'api/cadastro' => [
+            'file' => 'auth/cadastro.php',
+            'login' => false,
+            'grupos' => null
+        ]
+    ];
+
+    //Captura de URL
+    $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $route = trim($requestUri, '/');
+
+    //Processamento da rota
+    if (array_key_exists($route, $rotas)){
+        $configRota = $rotas[$route];
+        $file = $configRota['file'];
+    } else {
+        http_response_code(404);
+        echo json_encode(["status" => "erro", "mensagem" => "Endpoint não encontrado: " . $route]);
+        exit;
+    }
+
+    //Inclusão do arquivo do endpoint
+    $filePath = BASE_DIR . $file;
+
+    if (file_exists($filePath)){
+        include $filePath;
+    } else {
+        http_response_code(500);
+        echo json_encode(["status" => "erro", "mensagem" => "Arquivo interno ausente: {$file}"]);
+        exit;
+    }
+?>
